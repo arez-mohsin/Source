72969a93b66573deb362aaecb038919f965081f0
arez <77682696+arez-mohsin@users.noreply.github.com>
2022-01-08 22:23:10 +0300

    Update calli.js

--- calli.js
+++ calli.js
@@ -1586,4 +1586,17 @@ client.on("message", async (message) => {
     }
 });
 
+client.on('clickButton', async (button) => {
+    if (button.id === 'Button') { // You must enter the button ID in the 'Button' sections.
+        if (button.clicker.member.roles.cache.get("Roles ID")) {
+            await button.clicker.member.roles.remove("Roles ID")
+            button.defer(true)
+            await button.channel.send(`${button.clicker.user.tag} The role was taken because you clicked the button.!`).then(msg => { msg.delete({ timeout: 10000 }) }).catch(console.error);
+        } else {
+            await button.clicker.member.roles.add("Roles ID")
+            button.defer(true)
+            await button.channel.send(`${button.clicker.user.tag} The role was given because you clicked the button.`).then(msg => { msg.delete({ timeout: 10000 }) }).catch(console.error);
+        }
+    }
+});
 
